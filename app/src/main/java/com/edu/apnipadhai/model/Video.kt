6f33ffb288a2768af076bb8500b2e0c6622f6c44
vickyLongadge <vikramvik777@gmail.com>
2020-07-24 23:22:20 +0530

    video UI changed

--- app/src/main/java/com/edu/apnipadhai/model/Video.kt
+++ app/src/main/java/com/edu/apnipadhai/model/Video.kt
@@ -3,6 +3,10 @@ package com.edu.apnipadhai.model
 class Video {
     var id = 0
     var name = ""
+    var channel = "Channel Name"
+    var views = "views"
+    var uploaded = "2"
+    var duration = "00:00"
     var categoryId: String = "1"
     var videoId: String? = null
     var active: Boolean = true
--- app/src/main/java/com/edu/apnipadhai/ui/adapter/VideoAdapter.java
+++ app/src/main/java/com/edu/apnipadhai/ui/adapter/VideoAdapter.java
@@ -13,19 +13,21 @@ import com.edu.apnipadhai.R;
 import com.edu.apnipadhai.callbacks.ListItemClickListener;
 import com.edu.apnipadhai.model.Category;
 import com.edu.apnipadhai.model.Video;
+import com.google.firebase.database.snapshot.Index;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.RecordViewHolder> {
 
-    private static final String TAG = "SupplierAdapter";
     private final ListItemClickListener<Video> listener;
     private List<Video> list;
+    private OnChildItemClickListener menuListener;
 
-    public VideoAdapter(ListItemClickListener<Video> listener) {
+    public VideoAdapter(ListItemClickListener<Video> listener, OnChildItemClickListener menuListener) {
         this.list = new ArrayList<>();
         this.listener = listener;
+        this.menuListener = menuListener;
     }
 
 
@@ -45,10 +47,21 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.RecordViewHo
     @Override
     public void onBindViewHolder(@NonNull RecordViewHolder holder, int position) {
         Video vo = list.get(position);
-        holder.tvName.setText(vo.getVideoId());
+        holder.txtTitle.setText(vo.getVideoId());
+        StringBuilder builder = new StringBuilder();
+        builder.append(vo.getChannel()).append(" ").append(vo.getViews()).append(" ").append(vo.getUploaded()).append(" ").append("months ago");
+        holder.txtOther.setText(builder);
+        holder.txtDuration.setText(vo.getDuration());
         // holder.ivStatus.setImageResource(getStatusImg(vo.fetchStatus()));
         holder.itemView.setOnClickListener(v -> {
             listener.onItemClick(vo);
+        });
+
+        holder.imgMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menuListener.onChildItemClick(vo, holder.imgMore);
+            }
         });
     }
 
@@ -59,14 +72,19 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.RecordViewHo
 
 
     public static class RecordViewHolder extends RecyclerView.ViewHolder {
-        private AppCompatTextView tvName;
-        private AppCompatImageView ivStatus;
+        private AppCompatTextView txtTitle, txtOther,txtDuration;
+        private AppCompatImageView imgMore;
 
         public RecordViewHolder(@NonNull View itemView) {
             super(itemView);
-            tvName = itemView.findViewById(R.id.tvName);
-            // ivStatus = itemView.findViewById(R.id.ivStatus);
-
+            txtTitle = itemView.findViewById(R.id.txtTitle);
+            txtOther = itemView.findViewById(R.id.txtOther);
+            txtDuration = itemView.findViewById(R.id.txtDuration);
+            imgMore = itemView.findViewById(R.id.imgMore);
         }
+    }
+
+    public interface OnChildItemClickListener {
+        void onChildItemClick(Video video, AppCompatImageView view);
     }
 }
